0709ef3a414915b267575da9a706189c9e700abf
Viktoria Streltsova <viktostrel-ipt28@lll.kpi.ua>
2026-02-20 21:30:32 +0200

    Add files via upload
    Work on the encoder has been completed.

--- ITA_lab_1.cpp
+++ ITA_lab_1.cpp
@@ -5,7 +5,31 @@
 
 using namespace std;
 
-static const string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" "abcdefghijklmnopqrstuvwxyz" "0123456789+/";
+static const string b64_alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" "abcdefghijklmnopqrstuvwxyz" "0123456789+/";
+
+void encode_triplet(const unsigned char* triplet, char* target, int len) {
+    // 1-й символ: перші 6 біт першого байта
+    target[0] = b64_alphabet[(triplet[0] & 0xFC) >> 2];
+
+    // 2-й символ: 2 останні біти 1-го байта + 4 перші біти 2-го байта
+    target[1] = b64_alphabet[((triplet[0] & 0x03) << 4) | ((len > 1 ? (triplet[1] & 0xF0) >> 4 : 0))];
+
+    // 3-й символ: 4 останні біти 2-го байта + 2 перші біти 3-го байта
+    if (len > 1) {
+        target[2] = b64_alphabet[((triplet[1] & 0x0F) << 2) | ((len > 2 ? (triplet[2] & 0xC0) >> 6 : 0))];
+    }
+    else {
+        target[2] = '='; // Падінг <= був лише 1 байт
+    }
+
+    // 4-й символ: 6 останніх бітів 3-го байта
+    if (len > 2) {
+        target[3] = b64_alphabet[triplet[2] & 0x3F];
+    }
+    else {
+        target[3] = '='; // Падінг <= було менше 3 байтів
+    }
+}
 
 void encode_file(const string& input_path, const string& output_path) {
     ifstream in(input_path, ios::binary);
